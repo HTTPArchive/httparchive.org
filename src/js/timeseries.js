d6ae928079ad838923d1e1e8b6529c0e07984423
Rick Viscomi <rviscomi@users.noreply.github.com>
2018-10-27 17:55:59 -0400

    add a 3y option to timeseries (#101)

--- src/js/timeseries.js
+++ src/js/timeseries.js
@@ -347,6 +347,35 @@ function drawChart(options, series) {
 				${getChangelog(changelog)}`;
 			}
 		},
+		rangeSelector: {
+			buttons: [{
+		    type: 'month',
+		    count: 1,
+		    text: '1m'
+			}, {
+		    type: 'month',
+		    count: 3,
+		    text: '3m'
+			}, {
+		    type: 'month',
+		    count: 6,
+		    text: '6m'
+			}, {
+		    type: 'ytd',
+		    text: 'YTD'
+			}, {
+		    type: 'year',
+		    count: 1,
+		    text: '1y'
+			}, {
+		    type: 'year',
+		    count: 3,
+		    text: '3y'
+			}, {
+		    type: 'all',
+		    text: 'All'
+			}]
+		},
 		xAxis: {
 			type: 'datetime',
 			events: {
