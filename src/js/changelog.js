cb62730e49f0f56ba49c45bf9fa2b52cbf8456dc
Rick Viscomi <rviscomi@gmail.com>
2019-04-24 13:11:32 -0400

    use local changelog

--- src/js/changelog.js
+++ src/js/changelog.js
@@ -1,7 +1,7 @@
 export default class Changelog {
 
 	static get URL() {
-		return 'https://cdn.rawgit.com/HTTPArchive/httparchive/master/docs/changelog.json';
+		return '/static/json/changelog.json';
 	}
 
 }
