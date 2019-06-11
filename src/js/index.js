14abd04ec60621a05ca31b694df20a25f941e4a0
Andrew Noblet <andrewbnoblet@gmail.com>
2019-06-11 18:57:16 -0400

    Fix duplicate topics (#170)
    * Fix duplicate topics
    
    * Fix priority
    
    * Refactor
    
    * Fix errors
    
    * Remove console log
    
    * Be more specific as well as use set for instantiation
    
    Co-Authored-By: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Use foreach instead of map
    
    Co-Authored-By: Rick Viscomi <rviscomi@users.noreply.github.com>
    
    * Refactor
    
    * Format
    
    * Format
    
    * Formatting

--- src/js/index.js
+++ src/js/index.js
@@ -7,36 +7,31 @@ function getDiscussTopics() {
 	if (!section || !discussions) {
 		return;
 	}
+	let latestTopicIds = new Set();
 	fetch(`${Discussion.ORIGIN}/latest.json`).then(r => r.json()).then(r => {
 		const topics = r.topic_list.topics.slice(0, 2);
-		const allUsers = new Map(r.users.map(user => [user.id, user]));
-
-		if (topics && topics.length && allUsers.size) {
-			drawTopics(topics, allUsers);
-		}
+		topics.forEach(topic => latestTopicIds.add(topic.id));
+		drawTopics(topics, r.users);
 	}).then(_ => {
 		return fetch(`${Discussion.ORIGIN}/top.json`);
 	}).then(r => r.json()).then(r => {
-		const topics = r.topic_list.topics.slice(0, 3);
-		const allUsers = new Map(r.users.map(user => [user.id, user]));
-
-		if (topics && topics.length && allUsers.size) {
-			drawTopics(topics, allUsers);
-		}
-	}).then(_ => {
-		section.classList.remove('hidden');
-	});
+		const topics = r.topic_list.topics.filter(topic => !latestTopicIds.has(topic.id)).slice(0, 3);
+		drawTopics(topics, r.users);
+	}).then(_ => section.classList.remove('hidden'))
 }
 
-function drawTopics(topics, allUsers) {
-	topics.forEach(topic => {
-		const title = topic.title;
-		const slug = topic.slug;
-		const replies = topic.posts_count;
-		const users = topic.posters.map(poster => allUsers.get(poster.user_id));
+function drawTopics(topics, rUsers) {
+	const allUsers = new Map(rUsers.map(user => [user.id, user]));
+	if (topics && topics.length && allUsers.size) {
+		topics.forEach(topic => {
+			const title = topic.title;
+			const slug = topic.slug;
+			const replies = topic.posts_count;
+			const users = topic.posters.map(poster => allUsers.get(poster.user_id));
 
-		drawTopic(title, slug, replies, users);
-	});
+			drawTopic(title, slug, replies, users);
+		});
+	}
 }
 
 function drawTopic(title, slug, replies, users) {
