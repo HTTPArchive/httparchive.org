439e59ecf148953bd85700e38d502525be915607
Barry <barry@tunetheweb.com>
2021-04-25 22:36:37 +0100

    Revert "Upgrade Highcharts to latest version"
    This reverts commit 0c46c837499f477739cee37704f0b41aa20d42a3.

--- tools/generate/index.js
+++ tools/generate/index.js
@@ -1,10 +1,8 @@
 let { generate_sitemap } = require('./generate_sitemap');
-const { generate_js } = require('./generate_js');
 
 (async () => {
   try {
     await generate_sitemap();
-    await generate_js();
   } catch (error) {
     console.error(error);
     process.exit(1);
