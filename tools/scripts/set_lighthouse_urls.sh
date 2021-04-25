ef2855476f77a11bdeca7f724cc53c96d321cf43
Barry <barry@tunetheweb.com>
2021-04-25 18:41:16 +0100

    Tidy up

--- tools/scripts/set_lighthouse_urls.sh
+++ tools/scripts/set_lighthouse_urls.sh
@@ -56,7 +56,9 @@ elif [ "${RUN_TYPE}" == "pull_request" ] && [ "${COMMIT_SHA}" != "" ]; then
     git pull --quiet
     git checkout main
     # Then get the changes
-    CHANGED_FILES=$(git diff --name-only "main...${COMMIT_SHA}" --diff-filter=d content templates | grep -v base.html | grep -v main.html | grep -v ejs | grep -v base_ | grep -v sitemap | grep -v error.html | grep -v stories)
+    $(git diff --name-only "main...${COMMIT_SHA}" --diff-filter=d templates)
+    CHANGED_FILES=$(git diff --name-only "main...${COMMIT_SHA}" --diff-filter=d templates | grep -v base.html | grep -v main.html | grep -v ejs | grep -v base_ | grep -v sitemap | grep -v error.html )
+    echo "${CHANGED_FILES}"
     # Then back to the pull request changes
     git checkout --progress --force "${COMMIT_SHA}"
 
