0c46c837499f477739cee37704f0b41aa20d42a3
Barry <barry@tunetheweb.com>
2021-04-25 22:33:03 +0100

    Upgrade Highcharts to latest version

--- tools/generate/index.js
+++ tools/generate/index.js
@@ -1,8 +1,10 @@
 let { generate_sitemap } = require('./generate_sitemap');
+const { generate_js } = require('./generate_js');
 
 (async () => {
   try {
     await generate_sitemap();
+    await generate_js();
   } catch (error) {
     console.error(error);
     process.exit(1);
