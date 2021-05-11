f85239b9648bd7956697b4bd684ff78defff74c7
Barry <barry@tunetheweb.com>
2021-05-11 10:05:31 +0100

    Linting fixes

--- tools/scripts/test_reports.sh
+++ tools/scripts/test_reports.sh
@@ -81,6 +81,7 @@ fi
 # Export the number of fails to GitHub env
 if [[ "$GITHUB_ENV" ]]
 then
+    # shellcheck disable=SC1219
     echo "REPORT_TITLE=${TITLE}" >> "$GITHUB_ENV"
     echo "REPORT_FAILS=${FAIL}" >> "$GITHUB_ENV"
     echo "REPORT_FAIL_LOG<<EOF" >> "$GITHUB_ENV"
