7f1cfec216318aa8867620be2e04a3fd9ea03882
Barry Pollard <barrypollard@google.com>
2022-08-25 17:52:26 +0100

    Upgrade webvitals

--- src/js/send-web-vitals.js
+++ src/js/send-web-vitals.js
@@ -1,21 +1,82 @@
 function sendWebVitals() {
-  function sendWebVitalsGAEvents({name, delta, id}) {
+
+  function sendWebVitalsGAEvents({name, delta, id, attribution, navigationType}) {
+
+    let webVitalInfo = '(not set)';
+
+    switch (name) {
+      case 'CLS':
+        webVitalInfo = attribution.largestShiftTarget;
+        break;
+      case 'FID':
+        webVitalInfo = attribution.eventTarget;
+        break;
+      case 'LCP':
+        webVitalInfo = attribution.element;
+        break;
+      case 'TTFB':
+        webVitalInfo = attribution.connectionTime;
+        break;
+      case 'FCP':
+        webVitalInfo = attribution.firstByteToFCP;
+        break;
+      case 'INP':
+        webVitalInfo = attribution.eventTarget;
+        break;
+    }
+
+
+    // Measure some other user preferences
+    let dataSaver;
+    let effectiveType;
+    if ('connection' in navigator) {
+      dataSaver = navigator.connection.saveData.toString();
+      effectiveType = navigator.connection.effectiveType;
+    }
+    let deviceMemory;
+    if ('deviceMemory' in navigator) {
+      deviceMemory = navigator.deviceMemory.toString();
+    }
+    let prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches.toString();
+    let prefersColorScheme;
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      prefersColorScheme = 'dark';
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+      prefersColorScheme = 'light';
+    } else if (window.matchMedia('(prefers-color-scheme: no preference)').matches) {
+      prefersColorScheme = 'no preference';
+    } else {
+      prefersColorScheme = 'not supported';
+    }
+
+
     ga('send', 'event', {
       eventAction: name,
       eventCategory: 'Web Vitals',
       eventValue: Math.round(name === 'CLS' ? delta * 1000 : delta),
       eventLabel: id,
       nonInteraction: true,
+
+      // See: https://web.dev/debug-web-vitals-in-the-field/
+      dimension1: webVitalInfo,
+      dimension2: effectiveType,
+      dimension3: dataSaver,
+      dimension4: deviceMemory,
+      dimension5: prefersReducedMotion,
+      dimension6: prefersColorScheme,
+      dimension7: navigationType,
     });
+
   }
 
   // As the web-vitals script and this script is set with defer in order, so it should be loaded
   if (webVitals) {
-    webVitals.getFCP(sendWebVitalsGAEvents);
-    webVitals.getLCP(sendWebVitalsGAEvents);
-    webVitals.getCLS(sendWebVitalsGAEvents);
-    webVitals.getTTFB(sendWebVitalsGAEvents);
-    webVitals.getFID(sendWebVitalsGAEvents);
+    webVitals.onFCP(sendWebVitalsGAEvents);
+    webVitals.onLCP(sendWebVitalsGAEvents);
+    webVitals.onCLS(sendWebVitalsGAEvents);
+    webVitals.onTTFB(sendWebVitalsGAEvents);
+    webVitals.onFID(sendWebVitalsGAEvents);
+    webVitals.onINP(sendWebVitalsGAEvents);
   } else {
     console.error('Web Vitals is not loaded!!');
   }
--- tools/generate/generate_js.js
+++ tools/generate/generate_js.js
@@ -22,7 +22,7 @@ const generate_js = async () => {
   }
   console.log(`\n Generating web-vitals.js`);
   fs.copy(
-    './node_modules/web-vitals/dist/web-vitals.umd.js',
+    './node_modules/web-vitals/dist/web-vitals.attribution.umd.js',
     './static/js/web-vitals.js',
   );
 };
