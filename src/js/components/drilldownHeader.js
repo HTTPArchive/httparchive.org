8a2fb08181b8ecf1812885e1b1dc125906929ffc
Sarah Fossheim <sarah@fossheim.io>
2023-08-12 01:29:19 +0200

    improve styling, limit tags, limit tech selection for drilldown page

--- src/js/components/drilldownHeader.js
+++ src/js/components/drilldownHeader.js
@@ -5,20 +5,32 @@ function setTitle(title) {
 
 function setCategories(categories) {
   if(categories.length > 0) {
+    /* Fetch the category list element and empty it. */
     const list = document.querySelector('.intro .category-list');
     list.innerHTML = '';
-    categories.forEach((category)  => {
+
+    /* Get the first 5 elements of the array */
+    const _categories = categories.slice(0,5);
+    _categories.forEach((category)  => {
       const cellTemplate = document.getElementById('category-cell').content.cloneNode(true);
       cellTemplate.querySelector('li.cell').textContent = category;
       list.appendChild(cellTemplate);
     });
+
+    /* If there are more than 5 categories, show a message */
+    if(categories.length > 5) {
+      const more = categories.length - 5;
+      const cellTemplate = document.getElementById('category-cell').content.cloneNode(true);
+      cellTemplate.querySelector('li.cell').textContent = `+ ${more} more`;
+      list.appendChild(cellTemplate);
+    }
   }
 }
 
 function update(data, filters) {
   const app = filters.app[0];
   setTitle(app);
-  setCategories(data[app][0]?.category?.split(","));
+  setCategories(data[app][0]?.category?.split(", "));
 }
 
 export const DrilldownHeader = {
