6b24d967b3171e9e45e344a3d5e9ed27aed06d68
Sarah Fossheim <sarah@fossheim.io>
2024-10-18 02:39:56 +0200

    add checkboxes on category page, update texts, minor bugfixes

--- src/js/techreport/tableLinked.js
+++ src/js/techreport/tableLinked.js
@@ -7,12 +7,16 @@ class TableLinked {
     this.pageFilters = filters;
     this.submetric = ''; // TODO: Fetch the default one from somewhere
     this.data = data;
+    this.dataArray = Object.values(data) || [];
 
     this.updateContent();
   }
 
   // Update content in the table
   updateContent(content) {
+    // Temp, move to its own function
+    this.dataArray = Object.values(this.data);
+
     // Select a table based on the passed in id
     const component = document.getElementById(`table-${this.id}`);
     const tbody = component?.querySelector('tbody');
@@ -34,11 +38,43 @@ class TableLinked {
       const geo = filters.get('geo') || 'ALL';
       const rank = filters.get('rank') || 'ALL';
 
-      tableConfig.apps.forEach(app => {
-        const data = this.data[app];
+      // sort data
+      const sortEndpoint = component.dataset.sortEndpoint;
+      const sortMetric = component.dataset.sortMetric;
+      const sortKey = component.dataset.sortKey;
+      const client = component.dataset.client;
+
+      console.log(sortMetric);
+      if(sortMetric) {
+        this.dataArray = this.dataArray.sort((techA, techB) => {
+          // Sort techs by date to get the latest
+          const aSortedDate = techA.sort((a, b) => new Date(b.date) - new Date(a.date));
+          const bSortedDate = techB.sort((a, b) => new Date(b.date) - new Date(a.date));
+          const aLatest = aSortedDate[0];
+          const bLatest = bSortedDate[0];
+
+          // Get the correct endpoint & metric
+          const aMetric = aLatest[sortEndpoint].find(row => row.name === sortMetric);
+          const bMetric = bLatest[sortEndpoint].find(row => row.name === sortMetric);
+
+          const aValue = aMetric[client][sortKey];
+          const bValue = bMetric[client][sortKey];
+
+          return bValue - aValue > 0 ? 1 : -1;
+        });
+      }
+
+      this.dataArray.forEach(technology => {
+        // Set the data and the app name
+        const data = technology;
+        const app = technology[0].technology;
+        const formattedApp = DataUtils.formatAppName(app);
+
+        // Select the latest entry for each technology
         const sorted = data?.sort((a, b) => new Date(b.date) - new Date(a.date));
         const latest = sorted?.[0];
 
+        // If the latest entry exist, add it to the table
         if(latest) {
           const tr = document.createElement('tr');
 
@@ -47,10 +83,41 @@ class TableLinked {
             if(column.type === 'heading') {
               cell = document.createElement('th');
               const link = document.createElement('a');
-              const formattedApp = DataUtils.formatAppName(app);
               link.setAttribute('href', `/reports/techreport/tech?tech=${app}&geo=${geo}&rank=${rank}`);
               link.innerHTML = formattedApp;
               cell.append(link);
+            } else if (column.type === 'checkbox') {
+              cell = document.createElement('td');
+              const formattedApp = DataUtils.formatAppName(app);
+              const checkbox = document.createElement('input');
+              checkbox.setAttribute('type', 'checkbox');
+              checkbox.setAttribute('data-app', formattedApp);
+              checkbox.setAttribute('data-name', `table-${this.id}`);
+              checkbox.setAttribute('id', `${app}-table-${this.id}`);
+              checkbox.setAttribute('name', `${app}-table-${this.id}`);
+              checkbox.addEventListener('change', (e) => {
+                const appLinks = document.querySelectorAll('[data-name="selected-apps"]');
+                const selectedApps = document.querySelectorAll(`[data-name="table-${this.id}"]:checked`);
+
+                const selectedAppsFormatted = [];
+
+                selectedApps.forEach(selectedApp => {
+                  selectedAppsFormatted.push(selectedApp.dataset.app);
+                });
+
+                appLinks.forEach(appLinkEl => {
+                  appLinkEl.setAttribute('href', `/reports/techreport/tech?tech=${selectedAppsFormatted.join(',')}`);
+                  appLinkEl.innerHTML = `Compare ${selectedApps.length} technologies`;
+                });
+              });
+              cell.append(checkbox);
+
+              const label = document.createElement('label');
+              label.innerHTML = formattedApp;
+              label.classList.add('sr-only');
+              label.setAttribute('for', `${app}-table-${this.id}`);
+              cell.append(label);
+
             } else if(column.key === 'client') {
               cell = document.createElement('td');
               cell.innerHTML = component.dataset.client;
@@ -59,20 +126,23 @@ class TableLinked {
               const dataset = latest?.[column?.endpoint];
               let value = dataset?.find(entry => entry.name === column.subcategory);
               value = value?.[component.dataset.client]?.[column?.metric];
-              cell.innerHTML = `${value}`;
+              if(column.suffix) {
+                cell.innerHTML = `${value}${column.suffix}`;
+              } else {
+                cell.innerHTML = `${value}`;
+              }
             }
 
             if(column.className) {
               cell.className = column.className;
             }
 
-
-
             tr.append(cell);
           });
           tbody.append(tr);
         }
       });
+
     }
   }
 }
