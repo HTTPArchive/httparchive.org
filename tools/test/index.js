1d0e69343ff8c9503b616330c4ff885bdfa4f104
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2026-09-06 21:02:21 +0200

    feat: add Puppeteer-based console error test suite to the integration test runner
    Signed-off-by: Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>

--- tools/test/index.js
+++ tools/test/index.js
@@ -1,8 +1,10 @@
-let { test_status_codes } = require('./test_status_codes');
+const { test_status_codes } = require('./test_status_codes');
+const { test_console_errors } = require('./test_console_errors');
 
 (async () => {
   try {
     await test_status_codes();
+    await test_console_errors();
   } catch (error) {
     console.error(error);
     process.exit(1);
